ba8fe52c4329234fa46268a2e5d2d9e500b6876f
wayaka <suima4743@gmail.com>
2020-01-10 13:40:56 +0000

    add validation to admins

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def update_resource(resource, params)
+    # byebug
     resource.update_without_current_password(params)
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -18,4 +18,23 @@ class Admin < ApplicationRecord
     result
   end
   
+  validates :admin_name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 100 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  
+  validate :email_error_msg_make
+  
+  def email_error_msg_make
+    if errors[:email].any?
+      errors.full_messages.each do |msg|
+        if msg == "Emailを入力してください"
+          errors.messages.delete(:email)
+          errors.add(:email, "を入力してください")
+        end
+      end
+    end
+  end
+  
 end
