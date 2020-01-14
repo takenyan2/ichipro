b9a3d9066ac40195d64102d14c1f6c6c197f7953
wayaka <suima4743@gmail.com>
2020-01-14 19:09:58 +0000

    【WIP】add mail

--- app/mailers/add_reservation_mailer.rb
+++ app/mailers/add_reservation_mailer.rb
@@ -1,8 +1,10 @@
 class AddReservationMailer < ApplicationMailer
   default from: 'suima4743@gmail.com'
 
-  def add_reservation_mail(course_name)
+  def add_reservation_mail(course_name, reservation, course_time)
     @course_name = course_name
+    @reservation = reservation
+    @course_time = course_time
     mail(to: "wagamama2sre@gmail.com", subject: "ichiの予約が追加されました")
   end
 end
