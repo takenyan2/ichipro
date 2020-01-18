d9eb11956826ae5e85b5ad82d797e92e8d05964f
wayaka <suima4743@gmail.com>
2020-01-18 11:29:50 +0000

    コミットもれ

--- db/migrate/20190430092249_create_menus.rb
+++ db/migrate/20190430092249_create_menus.rb
@@ -2,7 +2,7 @@ class CreateMenus < ActiveRecord::Migration[5.2]
   def change
     create_table :menus do |t|
       t.string :menu_title
-      t.string :menu_image_id
+      t.string :image
       t.text :introduce
 
       t.timestamps
