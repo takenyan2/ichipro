283bbfbbf4880587a5bab7bf31fd1b5567b20db5
wayaka <suima4743@gmail.com>
2021-02-09 18:35:40 +0000

    sendgridバージョンアップ対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,8 +95,10 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
+  # :user_name => ENV['SENDGRID_USERNAME'],
+  # :password => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'apikey',
+  :password       => ENV['SENDGRID_API_KEY'],
   :domain => 'herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
